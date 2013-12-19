717b9ab54dfaffc32ac48eed369580d99655bb98
James Carragher <james@carragher.org>
2013-12-19 18:15:01 +0000

    Restructured data to new record all trials, added Gaussian function. To fix: delta function, saving new 'data' to database, rounding.

--- static/js/task.js
+++ static/js/task.js
@@ -18,8 +18,7 @@ psiTurk.preloadPages(pages);
 
 
 
-// Stimuli for a basic bandit experiment RAND INTS AND ASSIGN TO CARDS?
-var data = {};
+
 
 
 // Task object to keep track of the current phase
@@ -94,37 +93,104 @@ var Instructions = function(pages) {
 var TestPhase = function() {
     // Globals
     var cardSelected = false;
+    var data = {};
     var trial = 1;
     var maxTrial = 5;
     var condition= psiTurk.taskdata.get('condition');
 
-    // Function to generate numbers, stored in data
+    // Structure of data:
+    //  data ==== trialX ==== 'chosen_card'
+    //                   ==== 'chosen_value'
+    //                   ==== 'max_value'
+    //                   ==== 'trialNumber'
+    //                   ==== 'condition'
+    //                   ==== 'cardX' ==== 'R'
+    //                                ==== 'mu'
+
+    // Normal random number generator; Box-Muller transform (ignoring second random value returned 'y')
+    var rnd = function rnd(mean, stDev) {
+        var x = 0, y = 0, rds, c;
+
+        do {
+        x = Math.random()*2-1;
+        y = Math.random()*2-1;
+        rds = x*x + y*y;
+        }
+        while (rds == 0 || rds > 1)
+
+        c = Math.sqrt(-2*Math.log(rds)/rds);
+
+        return x * c * stDev + mean;
+    };
+
+    // Payout function:
+
+    // R[j](t) = mu[j](t) + epsilon[j](t)
+    // mu[j](t) = mu[j](t-1) - 6 * delta1[j](t-1) + 2 * delta2[j](t-1) + psi[j](t)
+
+    // Where:
+    // * R[j](t) is the reward of deck j on trial t
+    // * epsilon[j](t) is a random Normal variate with mean 0 and standard deviation sigma[e]
+    // * mu[j](t) is the mean reward of deck j on trial t
+    // * delta1[j](t-1) is an indicator function with value 1 if deck j was chosen on trial t-1 and 0 otherwise
+    // * delta2[j](t-1) is an indicator function with value 1 if deck j was not chosen on trial t-1 and 0 otherwise
+    // * psi[j](t) is a random Normal variate with mean 0 and standard deviation sigma[p]
+    // calcR argument is a string: 'cardX'
+    var calcR = function(card) {
+        // Indicator function assignment
+            // Will be NaN on first trial as no prior trials to compare to... Need to compare IDs in case of duplicate numbers
+            // Comparing int of html ID tag against string of data element, so need to strip string and turn to int
+            if (data[trial - 1]['chosen_card'] == data[trial - 1][parseInt(card.replace('card',''))]) {
+                var delta1 = 1;
+                var delta2 = 0;
+            }
+            else {
+                var delta1 = 0;
+                var delta2 = 1;
+            }
+        //data[trial][card]['mu'] = data[trial - 1][card]['mu'] - (6 * data[trial - 1][card]['delta1']) + (2 * data[trial - 1][card]['delta2']) + rnd(1, 1);
+        mu = data[trial - 1][card]['mu'] - (6 * delta1) + (2 * delta2) + rnd(1, 1);
+        //data[trial][card]['R'] = data[trial][card]['mu'] + rnd(1, 1);
+        R = mu + rnd(1, 1);
+        return {R: R,
+                mu: mu
+        };
+    };
+
+        // Function to generate numbers, stored in data
+//      var genNumbers = function() {
+//        data[trial] = {};
+//        data[trial]['card1'] = Math.floor(Math.random()*51);
+//        data[trial]['card2'] = Math.floor(Math.random()*51);
+//        data[trial]['card3'] = Math.floor(Math.random()*51);
+//        data[trial]['card4'] = Math.floor(Math.random()*51);
+//        return data[trial];
+//      };
+
+    //  Function to generate numbers, stored in data. Have to initialise all levels of data. New data[trial] made each trial
     var genNumbers = function() {
         data[trial] = {};
-        data[trial]['card1'] = Math.floor(Math.random()*51);
-        data[trial]['card2'] = Math.floor(Math.random()*51);
-        data[trial]['card3'] = Math.floor(Math.random()*51);
-        data[trial]['card4'] = Math.floor(Math.random()*51);
+        for (var i = 1; i <= 4; i++) {
+            data[trial]['card' + i] = {};
+            var values = calcR('card' + i);
+            data[trial]['card' + i]['R'] = values.R;
+            data[trial]['card' + i]['mu'] = values.mu;
+        }
         return data[trial];
     };
 
-    // Function used to clear p tags to prevent cheating. Do not clear data entirely-
-    // need record of previous trial for random walk
-    var genBlanks = function() {
-        data[trial] = {};
-        data[trial]['card1'] = 0;
-        data[trial]['card2'] = 0;
-        data[trial]['card3'] = 0;
-        data[trial]['card4'] = 0;
-        return data;
+    // Function to set cards to 0; to prevent cheating
+    var setBlanks = function() {
+        for (var i = 1; i <= 4; i++) {
+            $('#' + i).html('0');
+        }
     };
 
     // Function to set cards
     var setCards = function(values) {
-        $('#1').html(values['card1']);
-        $('#2').html(values['card2']);
-        $('#3').html(values['card3']);
-        $('#4').html(values['card4']);
+        for (var i = 1; i <= 4; i++) {
+            $('#' + i).html(values['card' + i]['R']);
+        }
     };
 
     var next = function() {
@@ -135,8 +201,27 @@ var TestPhase = function() {
             if (cardSelected == false) {
                 cardSelected = true;
 
-                // Initialise cards
+                // Initialise trial - 1 for the first trial to use. Must initialise all levels
+                if (trial == 1) {
+                    data[trial - 1] = {};
+                    // Arbitrary, set it to initial positions
+                    for (var i = 1; i <= 4; i++) {
+                        data[trial - 1]['card' + i] = {};
+                        data[trial - 1]['card' + i]['mu'] = 100;
+                        data[trial - 1]['card' + i]['R'] = 100;
+                    }
+                }
+
                 data[trial] = genNumbers();
+
+                // REMOVE BEFORE GOING LIVE
+                // For testing- iterate through data and print to console OLD DATA
+                for (var x in data[trial]) {
+                    console.log(x, data[trial][x])
+                }
+                console.log(trial);
+
+                // Set cards
                 setCards(data[trial]);
 
                 // Show card picked
@@ -146,7 +231,7 @@ var TestPhase = function() {
                 // Record meta-information
                 data[trial]['chosen_card'] = parseInt(card.attr('id'));
                 data[trial]['chosen_value'] = parseInt($('#' + data[trial]['chosen_card']).html());
-                data[trial]['max_value'] = Math.max(data[trial]['card1'], data[trial]['card2'], data[trial]['card3'], data[trial]['card4']);
+                data[trial]['max_value'] = Math.max(data[trial]['card1']['R'], data[trial]['card2']['R'], data[trial]['card3']['R'], data[trial]['card4']['R']);
                 data[trial]['trialNumber'] = trial;
                 data[trial]['condition'] = condition;
 
@@ -170,17 +255,16 @@ var TestPhase = function() {
                 for (var x in data[trial]) {
                         trialSet.push(data[trial][x]);
                     }
+                //No idea if works, you get the idea; iterate through: cards, then R and mu. Now debug.
+                for (var y = 1; y <= 4; y++) {
+                    for (var z in data[trial]['card' + y][z])
+                        trialSet.push(data[trial]['card' + y][z]);
+                    }
                 psiTurk.recordTrialData(trialSet);
 
                 // Forgone code
                 // Add delay to trials
                 setTimeout(function () {
-
-                    // REMOVE BEFORE GOING LIVE
-                    // For testing- iterate through data and print to console OLD DATA
-                    for (var x in data[trial]) {
-                        console.log(x, data[trial][x])
-                    }
 
                     // Re-hide all new cards and messages
                     $('._card p').hide();
@@ -196,8 +280,8 @@ var TestPhase = function() {
                     trial++;
 
                     // Create and assign new card values, record prior trial for dynamic element
-                    data[trial] = genBlanks();
-                    setCards(data[trial]);
+                    //console.log(data);
+                    setBlanks();
 
                     cardSelected = false;
 
